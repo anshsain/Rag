4dd3bd4c1aa69d756d8b5ed145ed26ec69024955
Ansh Saini <149475716+anshsain@users.noreply.github.com>
2026-01-16 13:35:47 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 import streamlit as st
-
+import cohere
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -31,6 +31,22 @@ if "embeddings" not in st.session_state:
     st.session_state.embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
     )
+    
+COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+co = cohere.Client(COHERE_API_KEY)
+
+def rerank_docs(query, docs, top_n=3):
+    texts = [doc.page_content for doc in docs]
+
+    rerank_results = co.rerank(
+        model="rerank-english-v3.0",
+        query=query,
+        documents=texts,
+        top_n=top_n,
+    )
+
+    reranked_docs = [docs[r.index] for r in rerank_results.results]
+    return reranked_docs
 
 # ------------------ INGEST ------------------
 
@@ -81,7 +97,9 @@ if st.button("Ask"):
         st.warning("Please ingest a document first.")
         st.stop()
 
-    docs = st.session_state.vectorstore.similarity_search(question, k=3)
+    retrieved_docs = st.session_state.vectorstore.similarity_search(question, k=8)
+    docs = rerank_docs(question, retrieved_docs, top_n=3)
+
 
     if not docs:
         st.warning("No relevant context found.")
